d0954e51695cc46745c06b575533be8e87faa047
florian <flodal974@gmail.com>
2023-04-22 11:32:44 +0200

    ajout transition sur btn

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const accueilSection = document.getElementById('accueil');
 const accueilswitchSection = document.getElementById('accueil-switch');
 
 btn.addEventListener('click', () => {
-    accueilSection.hidden = true;
-    accueilswitchSection.hidden = false;
+  // Ajouter la classe "transition" aux éléments que vous voulez animer
+  accueilSection.classList.add('transition');
+  accueilswitchSection.classList.add('transition');
+  
+  // Modifier les styles des éléments
+  accueilSection.style.opacity = 0;
+  accueilswitchSection.style.opacity = 0;
+  accueilswitchSection.removeAttribute('hidden');
+  setTimeout(() => {
+    accueilSection.style.display = 'none';
+    accueilswitchSection.style.opacity = 1;
+  }, 1000);
+  
+  // Supprimer la classe "transition" après un délai de 0,5 seconde pour que la transition ait lieu
+  setTimeout(() => {
+    accueilSection.classList.remove('transition');
+    accueilswitchSection.classList.remove('transition');
+  }, 2000);
 });
-
